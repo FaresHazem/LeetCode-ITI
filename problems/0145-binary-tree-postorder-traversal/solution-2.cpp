d50282a1e77da378724a3646f20e1cc9f402cded
Fares Hazem <fares.hzm12@gmail.com>
2025-12-29 17:48:53 +0200

    Day 16

---
+++ problems/0145-binary-tree-postorder-traversal/solution-2.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> arr;
+        stack<TreeNode*> st1, st2;
+        if (root != NULL) st1.push(root);
+
+        while(!st1.empty())
+        {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+
+            if (root->left != NULL) st1.push(root->left);
+            if (root->right != NULL) st1.push(root->right);
+        }
+        while(!st2.empty())
+        {
+            root = st2.top();
+            st2.pop();
+            arr.push_back(root->val);
+        }
+        return arr;
+    }
+};
