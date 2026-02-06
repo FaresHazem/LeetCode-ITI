d478557ef688c0155f7797dcfaf776fdec0492d6
Fares Hazem <fares.hzm12@gmail.com>
2026-02-06 13:27:32 +0200

    Day 47

---
+++ problems/0412-fizz-buzz/solution-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        for (int i = 1; i <= n; i++) {
+            string s = "";
+
+            if (i % 3 == 0) s += "Fizz";
+            if (i % 5 == 0) s += "Buzz";
+
+            if (s.empty())
+                s = to_string(i);
+
+            result.push_back(s);
+        }
+        
+        return result;
+    }
+};
